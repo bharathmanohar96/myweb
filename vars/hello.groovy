c1c612f9fa30f8e322b978ac333762203a4a85b8
bharathmanohar96 <102683198+bharathmanohar96@users.noreply.github.com>
2022-05-14 16:54:40 +0530

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,8 +1,7 @@
 def call(tomcatid,warname,credid) {
   sshagent(["${credid}"]) {
-                    // some block
-                    sh "scp -o StrictHostKeyChecking=no target/*.war ${tomcatid}:/home/ec2-user/tomcat8/webapps/${warname}.war"
-                    sh "ssh ${tomcatid} /home/ec2-user/tomcat8/bin/shutdown.sh"
-                    sh "ssh ${tomcatid} /home/ec2-user/tomcat8/bin/startup.sh"
-                }
+    sh "scp -o StrictHostKeyChecking=yes target/*.war ${tomcatid}:/home/ec2-user/tomcat8/webapps/${warname}.war"
+    sh "ssh ${tomcatid} /home/ec2-user/tomcat8/bin/shutdown.sh"
+    sh "ssh ${tomcatid} /home/ec2-user/tomcat8/bin/startup.sh"
+   }
 }
