ec2accb62e9fd042c4032d1567386f7669d26e38
bharathmanohar96 <102683198+bharathmanohar96@users.noreply.github.com>
2022-05-15 16:34:29 +0530

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -4,7 +4,7 @@ def call(tomcatid,warname,credid) {
     echo "${tomcatid}"
     echo "${warname}"
     sh "scp -o StrictHostKeyChecking=no target/*.war ec2-user@${tomcatid}:/home/ec2-user/tomcat8/webapps/${warname}.war"
-    sh "ssh ${tomcatid} /home/ec2-user/tomcat8/bin/shutdown.sh"
-    sh "ssh ${tomcatid} /home/ec2-user/tomcat8/bin/startup.sh"
+    sh "ssh ec2-user@${tomcatid} /home/ec2-user/tomcat8/bin/shutdown.sh"
+    sh "ssh ec2-user@${tomcatid} /home/ec2-user/tomcat8/bin/startup.sh"
    }
 }
